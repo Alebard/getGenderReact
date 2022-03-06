8a429cbfbbf06c9401f854dd8d34fe322bfc485b
Albert <albert_zteo@mail.ru>
2022-03-07 00:22:34 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import ReactDOM from 'react-dom'
 import React from "react";
+import {Answer, Form} from "./components"
 
 const serverUrl = 'https://api.genderize.io';
 
@@ -10,52 +10,28 @@ class App extends React.Component {
         super(props);
         this.state = {name: "", gender: ""}
 
-        this.setName = this.setName.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.Answer = this.Answer.bind(this);
     }
 
     async handleSubmit(e) {
         e.preventDefault();
-        console.log(this.state.name)
-        const urlCheckGender = `${serverUrl}?name=${this.state.name}`
+        const name = e.target[0].value;
+        const urlCheckGender = `${serverUrl}?name=${name}`
         const gender = (await fetch(urlCheckGender).then(response => response.json())).gender
-        console.log(gender)
+        this.setState({name: name});
         this.setState({gender: gender});
+
     }
 
-
-    setName(e) {
-        this.setState({name: e.target.value});
-    }
-
-
-    Answer() {
-        console.log(this.state.gender)
-        if (this.state.gender === null ) {
-            return (
-                <div>Неизвестное имя</div>
-            )
-        }else{
-            return (
-                this.state.gender
-            )
-        }
-    }
 
     render() {
         return (
             <div className="app">
-                <form onSubmit={this.handleSubmit}>
-                    <input id="name" type="text" placeholder='введите имя' onChange={this.setName}/>
-                    {this.state.name && this.state.name.length < 3 && <div>"Некорректное имя"</div>}
-                    <button>Проверить имя</button>
-                </form>
-                <this.Answer/>
+                <Form handleSubmit = {this.handleSubmit} setName = {this.setName} name = {this.state.name} />
+                <Answer gender = {this.state.gender}/>
             </div>
         )
     }
 }
-
 
 export default App
---
+++ src/components.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export function Answer(props) {
+    console.log(props.gender)
+    if (props.gender === null ) {
+        return (
+            <div>Неизвестное имя</div>
+        )
+    }else{
+        return (
+            props.gender
+        )
+    }
+}
+
+
+export function Form(props) {
+    const isNotValidName = props.name && props.name.length < 3
+    return(
+        <form onSubmit={props.handleSubmit}>
+            <input id="name" type="text" placeholder='введите имя'/>
+            { isNotValidName && <div>"Некорректное имя"</div>}
+            <button>Проверить имя</button>
+        </form>
+    )
+}
